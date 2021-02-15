0f2ae80a80caf2367b37ee6ec49e9451b931041f
ar78ilnik <arakchiev.ilya@yandex.ru>
2021-02-15 23:00:21 +0300

    Cards whyus

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ const csso = require('gulp-csso');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
 const autoprefixer = require('autoprefixer');
 const del = require('del');
 
@@ -18,10 +20,33 @@ function clean() {
     return del('dist');
 };
 
+function html() {
+    return gulp.src('app/**/*.html', {
+            base: 'app'
+        })
+        .pipe(gulp.dest('dist'))
+        .pipe(debug({
+            title: 'copy html'
+        }))
+};
+
+function toWebp() {
+    return gulp.src('app/img/**/*.{jpg,png}', {
+            base: 'app'
+        })
+        .pipe(webp({
+            quality: 90
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(debug({
+            title: 'toWebp'
+        }));
+};
+
 function copy() {
     return gulp.src([
             'app/fonts/**/*.*',
-            'app/img/**/*.{jpg,png,webp,mp4,svg}',
+            'app/img/**/*.{jpg,png,webp,svg}',
             'app/js/*.js'
         ], {
             base: 'app',
@@ -29,6 +54,20 @@ function copy() {
         .pipe(gulp.dest('dist'))
         .pipe(debug({
             title: 'copy'
+        }));
+};
+
+function sprite() {
+    return gulp.src('app/img/icon-*.svg', {
+            base: 'app'
+        })
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest('dist/img'))
+        .pipe(debug({
+            title: 'sprite'
         }));
 };
 
@@ -59,41 +98,32 @@ function style() {
         }))
 };
 
-function toHtml() {
-    return gulp.src('app/pages/index.pug')
-        .pipe(pug())
-        .pipe(debug({
-            title: 'pug'
-        }))
-        .pipe(gulp.dest('dist'))
-};
-
 function watch() {
-    gulp.watch('app/pages/**/*.pug', toHtml);
-    gulp.watch('app/pages/components/**/*.pug', toHtml);
+    gulp.watch('app/*.html', html);
     gulp.watch('app/scss/*.scss', style);
     gulp.watch('app/js/**/*.js', copy);
     gulp.watch('app/fonts/*.{woff, woff2}', copy);
-    gulp.watch('app/img/**/*.*', copy);
+    gulp.watch('app/img/**/*.*', copy, toWebp, sprite);
 };
 
 function server() {
     bs.init({
         server: 'dist'
     });
-    bs.watch('app/pages/**/*.pug').on('change', bs.reload);
+    bs.watch('app/*.html').on('change', bs.reload);
     bs.watch('app/components/**/*.pug').on('change', bs.reload);
     bs.watch('app/scss/*.scss').on('change', bs.reload);
     bs.watch('app/js/**/*.js').on('change', bs.reload);
     bs.watch('app/img/**/*.*').on('change', bs.reload);
-
 };
 
-const build = gulp.series(clean, copy, toHtml, style, gulp.parallel(watch, server));
+const build = gulp.series(clean, copy, toWebp, sprite, style, html, gulp.parallel(watch, server));
 
 exports.clean = clean;
+exports.html = html;
+exports.toWebp = toWebp;
+exports.sprite = sprite;
 exports.copy = copy;
-exports.toHtml = toHtml;
 exports.style = style;
 exports.build = build;
 exports.watch = watch;
